3da3c50b64b0a2e192ddd9d7847c24f607fe8bd9
Rafael Martinez <rterranova701@gmail.com>
2022-02-07 10:55:33 -0600

    NUT-66 Create specific components for Calculator and UploadPhoto

--- src/components/Buttons/index.js
+++ src/components/Buttons/index.js
@@ -1,18 +1,12 @@
 import { Button } from "reactstrap";
 import "./Buttons.scss";
 
-
-const Buttons = ({children,size,color,callback}) => {
-  //console.log(callback);
+const Buttons = ({ children, size, color }) => {
   return (
     <>
-      <Button 
-        className={color}
-        children={children}
-        size={size}
-        onClick={callback}>
-      </Button>
+      <Button className={color} children={children} size={size}></Button>
     </>
-  )};
+  );
+};
 
 export default Buttons;
--- src/pages/CreateRecipe/index.js
+++ src/pages/CreateRecipe/index.js
@@ -1,112 +1,12 @@
 import { Button, Form, FormGroup, Label, Input, FormText, FormFeedback } from "reactstrap";
 import { Container, Row, Col } from "reactstrap";
 import NavBar from "../../components/NavBar";
-import IngredientsDynamicTable from '../../components/IngredientsDynamicTable/'
-import NutFactTable from '../../components/NutFactTable/' 
+import Calculator from "../../components/Calculator";
+import UploadPhoto from "../../components/UploadPhoto";
 import { useState } from "react";
-import AWS from "aws-sdk";
-import Compressor from 'compressorjs';
 import "./CreateRecipe.scss";
 
 const CreateRecipe = () => {
-
-  const handleSubmit = (e) =>{
-    e.preventDefault()
-    console.log('Enviando...');
-  }
-
-  const [image, setImage] = useState("");
-  const [file, setFile] = useState("");
-  const [fileName, setFileName] = useState("");
-  const [fileType, setFileType] = useState("");
-  const [validateMainImage, setValidateMainImage] = useState(null);
-  const [thumbnail, setThumbnail] = useState(false);
-
-
-  //aws
-  AWS.config.update({
-    accessKeyId: process.env.REACT_APP_KEY_ID,
-    secretAccessKey: process.env.REACT_APP_SECRET_ACCESS_KEY,
-  });
-
-  const S3Client = new AWS.S3({
-    params: { Bucket: "kooben" },
-    region: "us-east-1",
-  });
-  //aws
-
-  const handleLoad = (e) => {
-    e.preventDefault();
-    const imageFile = e.target.files[0];
-    const imageUrl = URL.createObjectURL(imageFile);
-    compressImage(imageFile)
-    setImage(imageUrl)
-    const name = `k-${Date.now()}`
-    setFileName(name);
-    const type = imageFile.type;
-    setFileType(type);
-  };
-
-  const compressImage = (baseImage) => {
-    if (!baseImage) {
-      return;
-    }
-    new Compressor(baseImage, {
-      quality: 0.2,
-  
-      success(result) {
-        const formData = new FormData();
-        formData.append('file', result, result.name);
-        setFile(result)
-        setValidateMainImage('has-success')
-        setThumbnail(true)
-      },
-      error(err) {
-        console.log('error: ' + err.message);
-        setValidateMainImage('has-danger')
-        setThumbnail(false)
-      },
-    });
-  }
-
-  const handleDelete = async (e) => {
-    e.preventDefault();
-
-    const params = {
-      Key: `images/FB_IMG_1639091006023.jpg`,
-    };
-
-    try {
-      const response = await S3Client.deleteObject(params).promise();
-      console.log(response);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const handleSend = async (e) => {
-    e.preventDefault();
-    console.log("Sending to AWS...");
-
-    const params = {
-      ACL: "public-read",
-      Key: `images/${fileName}`,
-      ContentType: `${fileType}`,
-      Body: file,
-    };
-
-    try {
-      const response = await S3Client.putObject(params).promise();
-      console.log(response);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  console.log(file);
-  console.log(fileName);
-  console.log(fileType);
-
   return (
     <>
       <Container className="containerCreate" fluid>
@@ -122,7 +22,6 @@ const CreateRecipe = () => {
                 </Label>
                 <Col sm={10}>
                   <Input
-                    invalid
                     id="title"
                     name="title"
                     placeholder="escribe el título de tu receta..."
@@ -146,55 +45,12 @@ const CreateRecipe = () => {
                   />
                 </Col>
               </FormGroup>
+
+              <UploadPhoto />
               
-              <FormGroup className="imgContainer">
-                {thumbnail ? <img className="thumbnail" alt="tu receta" src={image} /> : ''}
-              </FormGroup>
-
-              <FormGroup row>
-                <Label for="mainPhoto" sm={2}>
-                  Fotografía:
-                </Label>
-                <Col sm={10}>
-                  <Input 
-                    valid={validateMainImage === 'has-success'}
-                    invalid={validateMainImage === 'has-danger'}
-                    id="mainPhoto"
-                    accept="image/png,image/jpeg"
-                    name="file"
-                    type="file"
-                    onChange={handleLoad}
-                  />
-                  <FormFeedback valid className="center">
-                    La imagen se cargó correctamente!
-                  </FormFeedback>
-                  <FormFeedback invalid className="center">
-                    Parece que el archivo que seleccionaste no es válido. Intenta con una imagen :)
-                  </FormFeedback>
-                  <FormText className="center">
-                    Agrega la fotografía principal de tu receta. Las imágenes serán
-                    optimizadas para web.
-                  </FormText>
-                </Col>
-              </FormGroup>
-        <Row className="ingredients boxCalculator">
-          <Col className="ingredientTable">
-              <FormGroup>
-                <IngredientsDynamicTable 
-                />
-              </FormGroup>
-          </Col>
-          <Col className="nutritionalTable">
-              <NutFactTable 
-              />
-          </Col>    
-        </Row>
-
-              <FormGroup>
-                <Button onClick={handleSend}>agrega foto</Button>
-
-                <Button onClick={handleDelete}>Borra foto</Button>
-              </FormGroup>
+              <Row className="ingredients boxCalculator">
+                <Calculator />
+              </Row>
 
             </Form>
           </Col>
