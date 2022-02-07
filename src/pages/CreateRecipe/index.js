f21d0871b026f9a61df60c6d3a8f241bb2291e39
Rafael Martinez <rterranova701@gmail.com>
2022-02-06 20:37:17 -0600

    NUT-66 compressing images and validate file-type

--- src/pages/CreateRecipe/index.js
+++ src/pages/CreateRecipe/index.js
@@ -1,31 +1,27 @@
-import {
-  Button,
-  Form,
-  FormGroup,
-  Label,
-  Input,
-  FormText,
-  FormFeedback,
-} from "reactstrap";
+import { Button, Form, FormGroup, Label, Input, FormText, FormFeedback } from "reactstrap";
 import { Container, Row, Col } from "reactstrap";
 import NavBar from "../../components/NavBar";
 import { useState } from "react";
 import AWS from "aws-sdk";
+import Compressor from 'compressorjs';
 import "./CreateRecipe.scss";
 
 const CreateRecipe = () => {
-  // const handleSubmit = (e) =>{
-  //   e.preventDefault()
-  //   console.log('Enviando...');
-  // }
+
+  const handleSubmit = (e) =>{
+    e.preventDefault()
+    console.log('Enviando...');
+  }
 
   const [image, setImage] = useState("");
   const [file, setFile] = useState("");
   const [fileName, setFileName] = useState("");
   const [fileType, setFileType] = useState("");
+  const [validateMainImage, setValidateMainImage] = useState(null);
+  const [thumbnail, setThumbnail] = useState(false);
+
 
   //aws
-
   AWS.config.update({
     accessKeyId: process.env.REACT_APP_KEY_ID,
     secretAccessKey: process.env.REACT_APP_SECRET_ACCESS_KEY,
@@ -35,26 +31,47 @@ const CreateRecipe = () => {
     params: { Bucket: "kooben" },
     region: "us-east-1",
   });
-
   //aws
 
   const handleLoad = (e) => {
     e.preventDefault();
     const imageFile = e.target.files[0];
     const imageUrl = URL.createObjectURL(imageFile);
+    compressImage(imageFile)
     setImage(imageUrl)
-    setFile(imageFile);
-    const name = imageFile.name;
+    const name = `k-${Date.now()}`
     setFileName(name);
     const type = imageFile.type;
     setFileType(type);
   };
 
+  const compressImage = (baseImage) => {
+    if (!baseImage) {
+      return;
+    }
+    new Compressor(baseImage, {
+      quality: 0.2,
+  
+      success(result) {
+        const formData = new FormData();
+        formData.append('file', result, result.name);
+        setFile(result)
+        setValidateMainImage('has-success')
+        setThumbnail(true)
+      },
+      error(err) {
+        console.log('error: ' + err.message);
+        setValidateMainImage('has-danger')
+        setThumbnail(false)
+      },
+    });
+  }
+
   const handleDelete = async (e) => {
     e.preventDefault();
 
     const params = {
-      Key: `images/FB_IMG_1639875646621.jpg`,
+      Key: `images/FB_IMG_1639091006023.jpg`,
     };
 
     try {
@@ -122,13 +139,13 @@ const CreateRecipe = () => {
                     id="synopsis"
                     name="synopsis"
                     type="textarea"
-                    placeholder="¡cuéntale al mundo porqué ésta receta es genial!"
+                    placeholder="¡cuéntale al mundo porqué tu receta es genial!"
                   />
                 </Col>
               </FormGroup>
 
               <FormGroup className="imgContainer">
-                <img className="thumbnail" alt="image" src={image} />
+                {thumbnail ? <img className="thumbnail" alt="image" src={image} /> : ''}
               </FormGroup>
 
               <FormGroup row>
@@ -136,17 +153,24 @@ const CreateRecipe = () => {
                   Fotografía:
                 </Label>
                 <Col sm={10}>
-                  <Input
+                  <Input 
+                    valid={validateMainImage === 'has-success'}
+                    invalid={validateMainImage === 'has-danger'}
                     id="mainPhoto"
                     accept="image/png,image/jpeg"
                     name="file"
                     type="file"
                     onChange={handleLoad}
                   />
+                  <FormFeedback valid className="center">
+                    La imagen se cargó correctamente!
+                  </FormFeedback>
+                  <FormFeedback invalid>
+                    Parece que el archivo que seleccionaste no es válido. Intenta con una imagen :)
+                  </FormFeedback>
                   <FormText>
-                    Agrega la fotografía principal de tu receta. Ten en cuenta
-                    que no debe ser mayor a XX tamaño. Los formatos permitidos
-                    son: .jpg, .jpeg, .png
+                    Agrega la fotografía principal de tu receta. Las imágenes serán
+                    optimizadas para web.
                   </FormText>
                 </Col>
               </FormGroup>
@@ -157,115 +181,6 @@ const CreateRecipe = () => {
                 <Button onClick={handleDelete}>Borra foto</Button>
               </FormGroup>
 
-              <FormGroup row>
-                <Label for="exampleEmail" sm={2}>
-                  Email
-                </Label>
-                <Col sm={10}>
-                  <Input
-                    id="exampleEmail"
-                    name="email"
-                    placeholder="with a placeholder"
-                    type="email"
-                  />
-                </Col>
-              </FormGroup>
-              <FormGroup row>
-                <Label for="examplePassword" sm={2}>
-                  Password
-                </Label>
-                <Col sm={10}>
-                  <Input
-                    id="examplePassword"
-                    name="password"
-                    placeholder="password placeholder"
-                    type="password"
-                  />
-                </Col>
-              </FormGroup>
-              <FormGroup row>
-                <Label for="exampleSelect" sm={2}>
-                  Select
-                </Label>
-                <Col sm={10}>
-                  <Input id="exampleSelect" name="select" type="select">
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
-                  </Input>
-                </Col>
-              </FormGroup>
-              <FormGroup row>
-                <Label for="exampleSelectMulti" sm={2}>
-                  Select Multiple
-                </Label>
-                <Col sm={10}>
-                  <Input
-                    id="exampleSelectMulti"
-                    multiple
-                    name="selectMulti"
-                    type="select"
-                  >
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
-                  </Input>
-                </Col>
-              </FormGroup>
-              <FormGroup row tag="fieldset">
-                <legend className="col-form-label col-sm-2">
-                  Radio Buttons
-                </legend>
-                <Col sm={10}>
-                  <FormGroup check>
-                    <Input name="radio2" type="radio" />{" "}
-                    <Label check>
-                      Option one is this and that—be sure to include why it's
-                      great
-                    </Label>
-                  </FormGroup>
-                  <FormGroup check>
-                    <Input name="radio2" type="radio" />{" "}
-                    <Label check>
-                      Option two can be something else and selecting it will
-                      deselect option one
-                    </Label>
-                  </FormGroup>
-                  <FormGroup check disabled>
-                    <Input disabled name="radio2" type="radio" />{" "}
-                    <Label check>Option three is disabled</Label>
-                  </FormGroup>
-                </Col>
-              </FormGroup>
-              <FormGroup row>
-                <Label for="checkbox2" sm={2}>
-                  Checkbox
-                </Label>
-                <Col
-                  sm={{
-                    size: 10,
-                  }}
-                >
-                  <FormGroup check>
-                    <Input id="checkbox2" type="checkbox" />{" "}
-                    <Label check>Check me out</Label>
-                  </FormGroup>
-                </Col>
-              </FormGroup>
-              <FormGroup check row>
-                <Col
-                  sm={{
-                    offset: 2,
-                    size: 10,
-                  }}
-                >
-                  <Button>Submit</Button>
-                </Col>
-              </FormGroup>
             </Form>
           </Col>
         </Row>
