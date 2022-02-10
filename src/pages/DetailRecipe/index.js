7dfcf327fd44d2fce3936e71e3b6de23a05e0f83
Luis Mora <luismora371@gmail.com>
2022-02-09 19:53:30 -0600

    NUT-67 updated index table

--- src/pages/DetailRecipe/index.js
+++ src/pages/DetailRecipe/index.js
@@ -1,129 +1,116 @@
 import MainSearchBar from "../../components/MainSearchBar"
 import IngredientsDynamicTable from "../../components/IngredientsDynamicTable"
-import NutFactTable from "../../components/NutFactTable"
+import NutFactTable from "../../components/NutFactTable";
 import { Container, Row, Col } from "reactstrap";
+import {useLocation} from 'react-router-dom';
 import "./DetailRecipe.scss";
 
 function DetailRecipe(){
-    const ingredients = [
-        {
-          name:"nopal 🌵",
-          equivalence:{
-            cup:5,
-            spoon:30,
-            piece:2,
-            gram:110
-          }
-        },
-        {
-          name:"jitomate 🍅",
-          equivalence:{
-            cup:4,
-            spoon:20,
-            piece:1,
-            gram:70
-          }
-        },
-        {
-          name:"cebolla 🧅",
-          equivalence:{
-            cup:3,
-            spoon:15,
-            piece:1,
-            gram:65
-          }
-        }
-      ]
+    const location = useLocation();
+    const ingredientes = location.state.recipe.metaData.ingredients
+    let TotalPortion = 0
 
+    const ingredientsTable = ingredientes.map((ingredient)=>{
+      const obj = {
+        name:`${ingredient.ingredient_id} 🌵`,
+        equivalence: ingredient.ingredient_equivalence
+      }
+      TotalPortion+= ingredient.ingredient_equivalence.gram
+      return obj
+    })
+
+    
+      console.log('******');
+      console.log(location)
     return(
         // <Container className="container" fluid>
         <div>
             
-            
             <div className="d-flex flex-column">
             <MainSearchBar></MainSearchBar>
                 <div class="p-2 mt-5">
-                    <h1 className="title">Nombre del Platillo</h1>
+                    <h1 className="title">{location.state.recipe.metaData.title}</h1>
                 </div>
                 <div class="p-2">
-                    <img src="https://recetinas.com/wp-content/uploads/2019/10/ensalada-de-nopales.jpg" width="400px"></img>
+                    <img src={location.state.recipe.metaData.url} width="400px"></img>
                 </div>
+                <div class="p-2"> Line Break</div>
                 <div class="p-2"> Line Break</div>
                 <div class="d-flex flex-row">
                     <div class="p-2">
                         <IngredientsDynamicTable
-                        ingredients={ingredients} 
+                        ingredients={ingredientsTable} 
                         ></IngredientsDynamicTable>
                     </div>
                     <div class="p-2">
                         <NutFactTable
-                            portion={300} 
-                            energy={100}
-                            carbohydrate={100}
-                            dvCarbohydrate={2}
-                            sugars={100}
-                            fiber={100}
-                            dvFiber={2}
-                            glycemic_load={10}
-                            protein={100}
-                            fat={100}
-                            dvFat={2}
-                            saturated_fatty_acids={100}
-                            dvSaturated_fatty_acids={2}
-                            monounsaturated_fatty_acids={100}
-                            dvMonounsaturated_fatty_acids={2}
-                            polyunsaturated_fatty_acids={100}
-                            dvPolyunsaturated_fatty_acids={2}
-                            cholesterol={100}
-                            dvCholesterol={2}
-                            sodium={100}
-                            dvSodium={2}
-                            calcium={100}
-                            dvCalcium={2}
-                            phosphorus={100}
-                            dvPhosphorus
-                            iron={100}
-                            dvIron={2}
-                            potassium={100}
-                            dvPotassium={2}
-                            magnesium={100}
-                            dvMagnesium={2}
-                            copper={100}
-                            dvCopper={2}
-                            zinc={100}
-                            dvZinc={2}
-                            manganese={100}
-                            dvManganese={2}
-                            selenium={100}
-                            dvSelenium={2}
-                            lithium={100}
-                            dvLithium={2}
-                            vitA={100}
-                            dvVitA={2}
-                            carotene={100}
-                            dvCarotene={2}
-                            bcarotene={100}
-                            dvBcarotene={2}
-                            vitB1={100}
-                            dvVitB1={2}
-                            vitB2={100}
-                            dvVitB2={2}
-                            vitB3={100}
-                            dvVitB3={2}
-                            vitB6={100}
-                            dvVitB6={2}
-                            vitB12={100}
-                            dvVitB12={2}
-                            vitC={100}
-                            dvVitC={2}
-                            folicAc={100}
-                            dvFolicAc={2}
-                            vitD={100}
-                            dvVitD={2}
-                            vitE={100}
-                            dvVitE={2}
-                            vitK={100}
-                            dvVitK={2}
+                            portion={TotalPortion} 
+                            energy={location.state.recipe.metaData.total_energy.quantity} 
+                            carbohydrate= {location.state.recipe.metaData.total_carbohydrate?.quantity} 
+                            dvCarbohydrate={location.state.recipe.metaData.CHANGE_ME?.quantity} 
+                            sugars= {location.state.recipe.metaData.total_sugars?.quantity} 
+                            fiber= {location.state.recipe.metaData.total_fiber?.quantity} 
+                            dvFiber={location.state.recipe.metaData.CHANGE_ME?.quantity} 
+                            glycemic_load={location.state.recipe.metaData.total_glycemic_load?.quantity} 
+                            protein= {location.state.recipe.metaData.total_protein?.quantity} 
+                            fat= {location.state.recipe.metaData.total_fat?.quantity} 
+                            dvFat={location.state.recipe.metaData.total_fat?.quantity}  // Esta es la que se muestra
+                            saturated_fatty_acids= {location.state.recipe.metaData.CHANGE_ME?.quantity} 
+                            dvSaturated_fatty_acids={location.state.recipe.metaData.CHANGE_ME?.quantity} 
+                            monounsaturated_fatty_acids= {location.state.recipe.metaData.CHANGE_ME?.quantity} 
+                            dvMonounsaturated_fatty_acids={location.state.recipe.metaData.CHANGE_ME?.quantity} 
+                            polyunsaturated_fatty_acids= {location.state.recipe.metaData.CHANGE_ME?.quantity} 
+                            dvPolyunsaturated_fatty_acids={location.state.recipe.metaData.CHANGE_ME?.quantity} 
+                            cholesterol= {location.state.recipe.metaData.total_cholesterol?.quantity} 
+                            dvCholesterol={location.state.recipe.metaData.CHANGE_ME?.quantity} 
+                            sodium= {location.state.recipe.metaData.total_sodium?.quantity} 
+                            dvSodium={location.state.recipe.metaData.CHANGE_ME?.quantity} 
+                            calcium= {location.state.recipe.metaData.CHANGE_ME?.quantity} 
+                            dvCalcium={location.state.recipe.metaData.CHANGE_ME?.quantity} 
+                            phosphorus= {location.state.recipe.metaData.CHANGE_ME?.quantity} 
+                            dvPhosphorus = {location.state.recipe.metaData.CHANGE_ME?.quantity} 
+                            iron= {location.state.recipe.metaData.CHANGE_ME?.quantity} 
+                            dvIron={location.state.recipe.metaData.CHANGE_ME?.quantity} 
+                            potassium= {location.state.recipe.metaData.CHANGE_ME?.quantity} 
+                            dvPotassium={location.state.recipe.metaData.CHANGE_ME?.quantity} 
+                            magnesium= {location.state.recipe.metaData.CHANGE_ME?.quantity} 
+                            dvMagnesium={location.state.recipe.metaData.CHANGE_ME?.quantity} 
+                            copper= {location.state.recipe.metaData.CHANGE_ME?.quantity} 
+                            dvCopper={location.state.recipe.metaData.CHANGE_ME?.quantity} 
+                            zinc= {location.state.recipe.metaData.CHANGE_ME?.quantity} 
+                            dvZinc={location.state.recipe.metaData.CHANGE_ME?.quantity} 
+                            manganese= {location.state.recipe.metaData.CHANGE_ME?.quantity} 
+                            dvManganese={location.state.recipe.metaData.CHANGE_ME?.quantity} 
+                            selenium= {location.state.recipe.metaData.CHANGE_ME?.quantity} 
+                            dvSelenium={location.state.recipe.metaData.CHANGE_ME?.quantity} 
+                            lithium= {location.state.recipe.metaData.CHANGE_ME?.quantity} 
+                            dvLithium={location.state.recipe.metaData.CHANGE_ME?.quantity} 
+                            vitA= {location.state.recipe.metaData.CHANGE_ME?.quantity} 
+                            dvVitA={location.state.recipe.metaData.CHANGE_ME?.quantity} 
+                            carotene= {location.state.recipe.metaData.CHANGE_ME?.quantity} 
+                            dvCarotene={location.state.recipe.metaData.CHANGE_ME?.quantity} 
+                            bcarotene= {location.state.recipe.metaData.CHANGE_ME?.quantity} 
+                            dvBcarotene={location.state.recipe.metaData.CHANGE_ME?.quantity} 
+                            vitB1= {location.state.recipe.metaData.CHANGE_ME?.quantity} 
+                            dvVitB1={location.state.recipe.metaData.CHANGE_ME?.quantity} 
+                            vitB2= {200} 
+                            dvVitB2={location.state.recipe.metaData.CHANGE_ME?.quantity} 
+                            vitB3= {location.state.recipe.metaData.CHANGE_ME?.quantity} 
+                            dvVitB3={location.state.recipe.metaData.CHANGE_ME?.quantity} 
+                            vitB6= {location.state.recipe.metaData.CHANGE_ME?.quantity} 
+                            dvVitB6={location.state.recipe.metaData.CHANGE_ME?.quantity} 
+                            vitB12= {300} 
+                            dvVitB12={location.state.recipe.metaData.CHANGE_ME?.quantity} 
+                            vitC= {location.state.recipe.metaData.CHANGE_ME?.quantity} 
+                            dvVitC={location.state.recipe.metaData.CHANGE_ME?.quantity} 
+                            folicAc= {location.state.recipe.metaData.CHANGE_ME?.quantity} 
+                            dvFolicAc={location.state.recipe.metaData.CHANGE_ME?.quantity} 
+                            vitD= {0} 
+                            dvVitD={location.state.recipe.metaData.CHANGE_ME?.quantity} 
+                            vitE= {location.state.recipe.metaData.CHANGE_ME?.quantity} 
+                            dvVitE={location.state.recipe.metaData.CHANGE_ME?.quantity} 
+                            vitK= {location.state.recipe.metaData.CHANGE_ME?.quantity} 
+                            dvVitK={location.state.recipe.metaData.CHANGE_ME?.quantity} 
                         />
                     </div>
                 </div>
