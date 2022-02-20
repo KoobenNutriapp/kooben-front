f913485c824fd3485f2b812c38413e2dc89a7c4a
Luis Mora <luismora371@gmail.com>
2022-02-19 18:52:38 -0600

    added environment urls

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,6 @@
-const BASE_URL = "http://localhost";
+// stage: prod | local
+const STAGE = 'prod' 
+const BASE_URL = (STAGE === 'prod' ? 'http://api.koo-ben.com' :"http://localhost");
 const PORT = 8080;
 const PATH_RECIPE = "/recipe";
 const PATH_INGREDIENT = "/ingredient";
