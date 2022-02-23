bec098d6b0b91b382878f13ff88568830c0fc205
Luis Mora <luismora371@gmail.com>
2022-02-23 11:29:07 -0600

    update

--- src/components/pdfCreationButton/index.js
+++ src/components/pdfCreationButton/index.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+// import * as React from 'react';
 import "./pdfCreationButton.scss"
 import pdfmakedownload from "../../services/PdfRecipeCreator";
 import { Button, Modal, ModalBody, ModalFooter } from "reactstrap";
